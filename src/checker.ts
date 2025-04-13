c03d7a2c164bee7e933be816b30bbcf530e7dc29
adel chemi <adelchemi6@gmail.com>
2025-04-13 01:22:55 +0100

    Refactor boycott logic to use dynamic datasets from JSON files

--- src/checker.ts
+++ src/checker.ts
@@ -1,17 +1,40 @@
-const boycottList = ["adidas", "nike"];
+import cloud from "../public/dataset/cloud.json";
+import commerce from "../public/dataset/commerce.json";
+import developer from "../public/dataset/developer.json";
+import finance from "../public/dataset/finance.json";
+import healthcare from "../public/dataset/healthcare.json";
+import hr from "../public/dataset/hr.json";
+import marketing from "../public/dataset/marketing.json";
+import others from "../public/dataset/others.json";
+import productivity from "../public/dataset/productivity.json";
+import sales from "../public/dataset/sales.json";
+import security from "../public/dataset/security.json";
+import web3 from "../public/dataset/web3.json";
+
+const allCompanies = [
+  ...cloud,
+  ...commerce,
+  ...developer,
+  ...finance,
+  ...healthcare,
+  ...hr,
+  ...marketing,
+  ...others,
+  ...productivity,
+  ...sales,
+  ...security,
+  ...web3,
+];
 
 export function checkWebsite(url: string): boolean {
-  try {
-    const parsedUrl = new URL(url);
-    const hostname = parsedUrl.hostname.toLowerCase();
+  const parsedUrl = new URL(url);
+  const hostname = parsedUrl.hostname.toLowerCase();
 
-    return boycottList.some((keyword) =>
-      hostname.includes(keyword.toLowerCase())
-    );
-  } catch (error) {
-    console.error("Invalid URL provided:", url);
-    return false;
-  }
+  const isBoycotted = allCompanies.some((keyword) =>
+    keyword.Website.includes(hostname.toLowerCase())
+  );
+
+  return isBoycotted;
 }
 
 export function checkAndUpdateBadge() {
